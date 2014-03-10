09b8af665b9a8e3d796a84a5f0e376d23177b5e0
Michael Shimmins <michael.shimmins@redsquirrel.com.au>
2014-03-10 16:08:16 +1100

    Added HEAD endpoint for Mandrill checks

--- app/controllers/cisco_controller.rb
+++ app/controllers/cisco_controller.rb
@@ -3,4 +3,8 @@ class CiscoController < ApplicationController
     # ugly debugging on heroku until we get a sample of the payload from mandrill
     throw params[:mandrill_events]
   end
+
+  def head_ise_guest
+    render :json => {:sucess => true}, :status => 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 TeamsquareSmsGateway::Application.routes.draw do
   post 'cisco/ise-guest' => 'cisco#ise_guest'
-
+  get 'cisco/ise-guest' => 'cisco#head_ise_guest'
 end
